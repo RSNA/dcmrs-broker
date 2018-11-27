fddb112de44a9d927b85f39725f8bed5a0fdd0bd
Andrew Bjonnes <andrew@abjonnes.com>
2018-11-27 12:23:10 -0500

    Add (very open) CORS headers

--- dcmrsbroker-core/src/main/java/org/rsna/isn/dcmrsbroker/core/spark/Router.java
+++ dcmrsbroker-core/src/main/java/org/rsna/isn/dcmrsbroker/core/spark/Router.java
@@ -60,6 +60,20 @@ public class Router implements SparkApplication
 		get(wadoBase + "/studies/:studyUid", new WadoRoute(Level.STUDY));
 		get(wadoBase + "/studies/:studyUid/series/:seriesUid", new WadoRoute(Level.SERIES));
 		get(wadoBase + "/studies/:studyUid/series/:seriesUid/instances/:instanceUid", new WadoRoute(Level.IMAGE));
+
+        options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null)
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null)
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+
+            return "OK";
+        });
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
 	}
 
 	@Override
